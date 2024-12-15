a78ee2e6b9fd9c08e02a872dc55c49026edd3125
heyiming <ymjrcc@qq.com>
2024-12-15 21:17:07 +0800

    Initial commit.

---
+++ src/my-icp-todo-frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { my_icp_todo_backend } from 'declarations/my-icp-todo-backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    my_icp_todo_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
